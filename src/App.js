17fb9aa566452c214bbc50e5f9bf74dd5a1dbc18
Harry Zhang <harryzhang100@gmail.com>
2020-10-08 00:56:35 -0700

    created general layout of the image upload page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Box, Button, Image, Flex, Text } from "@chakra-ui/core";
 import './App.css';
 
 function App() {
+  // store the file object an
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Flex align="center" justify="center" direction="column" 
+        border="1px" borderColor="#171923" minHeight="100vh">
+        <Text fontSize="2xl">Upload an Image</Text>
+        <Text fontSize="xl">File should be .jpeg, .png...</Text>
+        <Box bg="#90CDF4" w="80%" color="grey">
+          <Image src="gibbresh.png" fallbackSrc="https://via.placeholder.com/150" />
+          <Text fontSize="xl">Drag and Drop Your Image Here</Text>
+        </Box>  
+        <Text fontSize="xl">
+          OR
+        </Text>
+        <Button variantColor="blue" variant="solid">
+          Choose a File
+        </Button>
+      </Flex>
     </div>
   );
 }
